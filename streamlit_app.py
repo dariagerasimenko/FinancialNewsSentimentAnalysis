d47a53fe1f106761df1a9b3e5308f0f5fddd3b9c
Daria Gerasimenko <daksagerasimenko@gmail.com>
2024-07-07 20:56:55 +0200

    i commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,9 @@ import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
-sentiment  = {'positive': 0,'neutral': 1,'negative':2}
+sentiment = {'positive': 0,'neutral': 1,'negative':2}
 model = load_model('Mymodel.h5')
-
-message = ['Apple is doing very well in this lousy economy.']
-seq = tokenizer.texts_to_sequences(message)
-
-padded = pad_sequences(seq, maxlen=50, dtype='int32', value=0)
-
-pred = model.predict(padded)
-print(message[0])
-labels = ['positive','neutral','negative']
-print(f"probability of positive class: {pred[0][sentiment['positive']]}")
-print(f"probability of neutral class: {pred[0][sentiment['neutral']]}")
-print(f"probability of negative class: {pred[0][sentiment['negative']]}")
-print(f"predicted class: {labels[np.argmax(pred)]}")
-
+labels = ['positive', 'neutral', 'negative']
 st.title("RNN example : Financial News Sentiment Analysis")
 st.write(
     "write the sentence to get an opinion"
@@ -28,5 +15,16 @@ st.write(
 input_text = st.text_area("Enter your text here:")
 # Display the entered text
 if input_text:
-    st.write("You entered:")
-    st.write(input_text)
+
+    message = input_text
+    seq = tokenizer.texts_to_sequences(message)
+
+    padded = pad_sequences(seq, maxlen=50, dtype='int32', value=0)
+
+    pred = model.predict(padded)
+    st.write(f"probability of positive class: {pred[0][sentiment['positive']]}")
+    st.write(f"probability of neutral class: {pred[0][sentiment['neutral']]}")
+    st.write(f"probability of negative class: {pred[0][sentiment['negative']]}")
+    st.write(np.argmax(pred))
+    st.write(f"predicted class: {labels[np.argmax(pred)]}")
+
