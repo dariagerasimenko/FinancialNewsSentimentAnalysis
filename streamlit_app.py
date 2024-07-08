f225206677400d7f636dc0fbce2c69374bafc336
Daria Gerasimenko <daksagerasimenko@gmail.com>
2024-07-08 19:58:48 +0200

    added colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ with open('trained_model/tokenizer.pickle', 'rb') as handle:
 sentiment = {'Positive': 0,'Neutral': 1,'Negative':2}
 model = load_model('trained_model/Mymodel.h5')
 labels = ['Positive', 'Neutral', 'Negative']
-st.title("RNN example : Financial News Sentiment Analysis")
+st.title("LSTM: Financial News Sentiment Analysis")
 st.write(
     "by Daria Gerasimenko, Student of HHU"
 )
@@ -26,5 +26,12 @@ if input_text:
     st.write(f"Probability of positive class: {round(pred[0][sentiment['Positive']]*100)} %")
     st.write(f"Probability of neutral class: {round(pred[0][sentiment['Neutral']]*100)} %")
     st.write(f"Probability of negative class: {round(pred[0][sentiment['Negative']]*100)} %")
-    st.write(f"Predicted class: {labels[np.argmax(pred)]}")
+    predicted_class = labels[np.argmax(pred)]
+
+    # Color coding for predicted class
+    color_dict = {'Positive': 'green', 'Neutral': 'blue', 'Negative': 'red'}
+    color = color_dict[predicted_class]
+
+    # Display the predicted class with color
+    st.markdown(f"<span style='color:{color}'>Predicted class: {predicted_class}</span>", unsafe_allow_html=True)
 
